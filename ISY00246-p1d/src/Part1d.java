1aec9a23174a8602ed9678e092512e44697a9135
Dan <dan1ate2@gmail.com>
2015-08-13 15:19:11 +1000

    Added part 1d files

---
+++ ISY00246-p1d/src/Part1d.java
@@ -0,0 +1,15 @@
+/*
+    Dan Hogan
+    ISY00246
+    Assignment 1
+    Part 1d
+    Purpose: Test input of Stock data from file
+ */
+
+public class Part1d {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ ISY00246-p1d/src/Stock.java
@@ -0,0 +1,18 @@
+/*
+    Dan Hogan
+    ISY00246
+    Assignment 1
+    Part 1d
+    Purpose: Stock interface
+ */
+
+public interface Stock {
+    public int getID();
+        // get stock ID
+    public String getDescription();
+        // get stock description
+    public int number();
+        // get number of items
+    public void setNumber(int num);
+        // update number (quantity) in stock
+}
