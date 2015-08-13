791a5ac9786dbc591d75993027cda10b50001dfb
Dan <dan1ate2@gmail.com>
2015-08-13 15:07:25 +1000

    Part c working

---
+++ ISY00246-p1c/src/part1c/Part1c.java
@@ -0,0 +1,35 @@
+/*
+    Dan Hogan
+    ISY00246
+    Assignment 1
+    Part 1c
+    Purpose: Test polymorphism on stock items
+ */
+package part1c;
+
+import java.util.ArrayList;
+
+public class Part1c {
+
+    public static void main(String[] args) {
+        
+        // create array list of stock
+        ArrayList<Stock> stockList = new ArrayList<Stock>() {};
+        stockList.add(new Confectionary
+            (11, "Red Liqorice", 5, "23rd June 2016"));
+        stockList.add(new Confectionary
+            (12, "Jelly Beans", 34, "15th December 2017"));
+        stockList.add(new SoftDrink(21, "Sprite", 3, 12));
+        stockList.add(new SoftDrink(22, "Coke", 5, 12));
+        
+        // print stock items in arraylist
+        for (Stock i : stockList) {
+            displayStockItem(i);
+        }
+    }
+    
+    // polymorphically process Stock items for print
+    public static void displayStockItem(Stock s) {
+        System.out.println(s);
+    }
+}
