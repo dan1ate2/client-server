e1110be920c5e537a6697a6122a2396afeb9a4a9
Dan <dan1ate2@gmail.com>
2015-08-15 12:47:06 +1000

    Part1d reading file ok

--- A1P1d/src/Part1d.java
+++ A1P1d/src/Part1d.java
@@ -1,20 +1,34 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Dan Hogan
+    ISY00246
+    Assignment 1
+    Part 1d
+    Purpose: Test reading file with Stock object data
  */
 
-/**
- *
- * @author Ðan
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Part1d {
-
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        System.out.println(args[0]);
+    public static void main(String[] args) throws IOException {
+        System.out.println(args[0]); // prints argument
+        
+        if (args.length > 0) { // if argument exists
+            
+            // try print file
+            File file = new File(args[0]);
+            if (file.exists()) {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                for (String line; (line = br.readLine()) != null;) {
+                    System.out.print(line);
+                }
+            }
+        }
     }
-    
 }
