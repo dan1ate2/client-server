3b93016e8e4d045cc9dc42486e56f3e21b2df0af
dan1ate2 <dan1ate2@gmail.com>
2015-08-20 11:49:36 +1000

    Part3b small logic error in client with validating data. Need to create function with the ability to read console too

--- A1P3b/src/NameClient.java
+++ A1P3b/src/NameClient.java
@@ -1,4 +1,3 @@
-// THIS WILL BE BRANCH??
 /*
     Dan Hogan
     ISY00246
@@ -10,7 +9,6 @@ import java.io.*;
 import java.net.*;
 
 public class NameClient {
-    private static final int CLIENT_PORT = 2014;
     private static final int SERVER_PORT = 2015;
     
     public static void main(String[] args) 
@@ -18,39 +16,82 @@ public class NameClient {
         
         String inConsoleString;
         String inServerString;
+        boolean validOption;
+        
         
         InetAddress server = InetAddress.getByName(args[0]); // server address
         try (Socket sock = new Socket(server, SERVER_PORT)) { // create socket
-        
             System.out.println("Connected to " + args[0]); // print connection
             
-            // setup stream output to server
-            PrintStream outStream = new PrintStream(sock.getOutputStream());
+            // output to server
+            PrintStream outStream = 
+                    new PrintStream(sock.getOutputStream()); 
             
-            // setup stream input from server
+            // input from server
             BufferedReader inStream =
                     new BufferedReader(
                             new InputStreamReader(sock.getInputStream()));
             
-            // setup user console input
+            // user console input
             BufferedReader inConsole =
                     new BufferedReader(
                             new InputStreamReader(System.in));
             
-                // TEST GET INPUT
-                System.out.print("Enter something: ");
-                inConsoleString = inConsole.readLine();
-
-                // TEST SEND TO SERVER
-                outStream.println(inConsoleString);
-                outStream.flush();
-
-                // TEST RECEIVE FROM SERVER
-                inServerString = inStream.readLine();
-                System.out.println("Server said: "+inServerString);
+            displayMenu();
+            inConsoleString = inConsole.readLine(); // get user option/input
             
+            // validate menu option
+            do {
+                validOption = validateMenuOption(inConsoleString);
+                if (!validOption) {
+                    displayMenu();
+                    inConsoleString = inConsole.readLine(); // get user option/input
+                }
+            } while (!validOption);
+            
+            while (!inConsoleString.equals("5")) { // loop until exit option
+                outStream.println(inConsoleString); // set console input for send
+                outStream.flush(); // send to server
+                inServerString = inStream.readLine(); // get from server
+                System.out.println("Server received: "+inServerString);
+
+                displayMenu(); // displays menu
+                inConsoleString = inConsole.readLine(); // get user option/input
+            }
+            
+            outStream.println();   // send blank line to tell server we are done
+            outStream.flush(); // send to server
             sock.close(); // close socket
-        } // end try socket
+        } // end try socket // end try socket // end try socket // end try socket
     } // end main method
+    
+    // display the main menu
+    public static void displayMenu() {
+        System.out.print("\nName Saver Server Menu\n\n"
+                +"1. Add a name\n2. Remove a name\n3. List all names"
+                +"4. Check if name recorded\n5. Exit\n\n"
+                +"Enter selection [1-5]:");
+    }
+    
+    //validate menu choice
+    public static boolean validateMenuOption(String input) {
+        boolean valid = false;
+        
+        try {
+            if (Integer.parseInt(input) < 1 || Integer.parseInt(input) > 5){
+                System.out.println("Menu option not valid.\n"
+                        +"Please type a single digit for menu option.");
+            } 
+            else {
+                valid = true;
+            }
+        }
+        catch (NumberFormatException nfe) {
+            System.out.println("Error, only a single number can be input.\n"
+            +"Please type a single digit for menu option.");
+        }
+  
+        return valid;
+    }
     
 } // end class
