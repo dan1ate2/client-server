428becec8ab7df4dab974b100402ac42433ebfb8
dan1ate2 <dan1ate2@gmail.com>
2015-09-27 18:39:49 +1000

    Just got xml files loaded, fixing up errors

--- A2P1/src/Part1/Part1.java
+++ A2P1/src/Part1/Part1.java
@@ -11,20 +11,29 @@ import org.xml.sax.SAXException;
 public class Part1 {
 
     public static void main(String[] args) {
-        Source part1Schema = null;
         
         try {
-            File xmlFile = new File("part1.xml");
+            File xmlFile = new File("src/Part1/part1.xml");
+            File schemaFile = new File("src/Part1/part1.xsd");
+
             DocumentBuilderFactory factory =
                     DocumentBuilderFactory.newInstance();
+            
             SchemaFactory sfactory = 
                 SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = sfactory.newSchema(part1Schema);
+            Schema schema = sfactory.newSchema(schemaFile);
+            
             factory.setSchema(schema);
             DocumentBuilder builder =
                 factory.newDocumentBuilder();
+            
             Document doc = builder.parse(xmlFile);
-            }
+            Element root = doc.getDocumentElement();
+
+            String format = root.getAttribute("id");
+             if (format != null)
+                 System.out.println("attribute ID: "+format);
+                }
         catch (SAXException se) {
             System.err.println(se.getMessage());
         }
