a575bffa75b6430ee077216a903359c10980435d
dan1ate2 <dan1ate2@gmail.com>
2015-09-30 01:28:43 +1000

    Finished part 2

--- A2P2/src/Part2/Part2.java
+++ A2P2/src/Part2/Part2.java
@@ -19,18 +19,12 @@ import org.xml.sax.SAXException;
 public class Part2 {
 
     public static void main(String[] args) {
-//        if (args.length != 2) {
+//        if (args.length != 1) {
 //            System.out.println("Requires team xml file as arg");
 //            System.exit(-1); // exit program
 //         }
         try {
-            BufferedReader in =
-                new BufferedReader(
-                    new InputStreamReader(System.in)); // console input
-            
-            // welcome message
-            System.out.println("This program Print out details/fields of a team xml file.");
-            // get files
+            // get file
             File xmlFile = new File("src/Part2/part2.xml");
             // dom parser
             DocumentBuilderFactory factory =
@@ -39,40 +33,90 @@ public class Part2 {
                 factory.newDocumentBuilder();
             // parse xml
             Document doc = builder.parse(xmlFile);
-
+            // get root element
             Element root = doc.getDocumentElement();
-            NodeList ns = root.getChildNodes();
-            for (int i=0; i<ns.getLength(); i++) {
-                if (ns.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                    Element el = (Element) ns.item(i);
-                    if (el.getTagName().equals("teamDetail")) {
-                        Text str = (Text) el.getFirstChild();
-                        System.out.println("element: "+str.getWholeText());
+            // get child elements of root
+            NodeList nl = root.getChildNodes();
+            Integer numTeamMembers = 0; // member counter
+            for (int i = 0; i < nl.getLength(); i++) {
+                if (nl.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    Element elm = (Element) nl.item(i);
+                    // if teamDetails
+                    if (elm.getTagName().equals("teamDetail")) {
+                        Element fullname = 
+                            (Element) elm.getElementsByTagName("fullname").item(0);
+                        System.out.println("\nTeam details for \""
+                            +fullname.getTextContent()+"\" (code "
+                            +elm.getAttribute("id")+"):\n");
                     }
-//                    else if (el.getTagName().equals("address")) {
-//                        NodeList adrs = el.getChildNodes();
-//                        for (int j=0; j<adrs.getLength(); j++) {
-//                            if (adrs.item(j).getNodeType() == Node.ELEMENT_NODE) {
-//                                Element elt2 = (Element) adrs.item(j);
-//                                if (elt2.getTagName().equals("street")) {
-//                                    Text str = (Text) elt2.getFirstChild();
-//                                    System.out.println("street: "+str.getWholeText());
-//                                }
-//                                else if (elt2.getTagName().equals("city")) {
-//                                    Text str = (Text) elt2.getFirstChild();
-//                                    System.out.println("City: "+str.getWholeText());
-//                                    System.out.println("Postcode: "+elt2.getAttribute("postcode"));
-//                                }
-//                            }// end if
-//                        } // end for
-//                    } // end elseif
-                } // end if
+                    else if (elm.getTagName().equals("member")) {
+                        String memberName = elm.getAttribute("name");
+                        String position = elm.getAttribute("position");
+                        String mailAddress = "";
+                        String altPosition = "";
+                        String feesPaid = "";
+                        // get member child nodes
+                        NodeList memberChildNodes = elm.getChildNodes();
+                        // set member variables
+                        for (int n = 0; n < memberChildNodes.getLength(); n++) {
+                            if (memberChildNodes.item(n).getNodeType() 
+                                == Node.ELEMENT_NODE) {
+                                Element memberElm = (Element) memberChildNodes.item(n);
+                                if (memberElm.getTagName().equals("mailAddress")) {
+                                    mailAddress = memberElm.getTextContent();
+                                }
+                                else if (memberElm.getTagName().equals("altPosition")) {
+                                    altPosition = memberElm.getTextContent();
+                                }
+                                else if (memberElm.getTagName().equals("feesPaid")) {
+                                    feesPaid = memberElm.getTextContent();
+                                }
+                            }
+                        } // end for
+                        // print 'players' title before first member
+                        if (numTeamMembers == 0) {
+                           System.out.println("Players:");
+                        }
+                        // print member details
+                        if ("".equals(altPosition)) {
+                        System.out.println(memberName+", "+position+" (Fees paid: $"+feesPaid+")\n\t"
+                            +mailAddress);
+                        }
+                        else {
+                            System.out.println(memberName+", "+position+" (Fees paid: $"+feesPaid+")\n\t"
+                            +mailAddress+"\n\tAlt position: "+altPosition);
+                        }
+                        numTeamMembers++;
+                    } // end else if member
+                } // end if element
+                // print team contact if last node (last member printed)
+                if ((i + 1) == nl.getLength()) {
+                    Element teamDetail = 
+                        (Element) root.getElementsByTagName("teamDetail").item(0);
+                    String leaderName = "";
+                    String leaderPhone = "";
+                    String leaderEmail = "";
+                    NodeList teamDetailNodes = teamDetail.getChildNodes();
+                    for (int j = 0; j < teamDetailNodes.getLength(); j++) {
+                        if (teamDetailNodes.item(j).getNodeType() 
+                            == Node.ELEMENT_NODE) {
+                            Element teamElm = (Element) teamDetailNodes.item(j);
+                            if (teamElm.getTagName().equals("leader")) {
+                                // set leader detail variables
+                                leaderName = 
+                                    teamElm.getElementsByTagName("name").item(0).getTextContent();
+                                leaderPhone = 
+                                    teamElm.getElementsByTagName("phone").item(0).getTextContent();
+                                leaderEmail = 
+                                    teamElm.getElementsByTagName("email").item(0).getTextContent();
+                            }
+                        }
+                    }
+                    System.out.println("\nTotal of "+numTeamMembers+" team member(s).\n"
+                        +"Team contact "+leaderName+" "+leaderPhone+", "
+                        +leaderEmail+"\n");
+                } // end if last node
             } // end for
-            
-            // print confirmation
-            System.out.println("Press any key to exit.");
-            in.readLine();
-            System.exit(-1); // exit program
         } // end try
         catch (SAXException se) {
             System.err.println(se.getMessage());
